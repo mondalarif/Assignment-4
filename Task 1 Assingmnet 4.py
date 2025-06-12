c976bbfde09ddca0bd4c52d8a2f31d2c9aad9e6e
mondalarif <mondalarif0101@gmail.com>
2025-06-12 20:38:34 +0530

    Add files via upload

---
+++ Task 1 Assingmnet 4.py
@@ -0,0 +1,11 @@
+try:
+    with open("sample.txt", "r") as file:
+        print("Reading file content:")
+        line_number = 1
+        for line in file:
+            print(f"Line {line_number}: {line.strip()}")
+            line_number += 1
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
+
+# I am confused with task 2 of assingment 4 
